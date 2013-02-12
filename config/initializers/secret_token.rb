4ea721ecf4bc98cee7962166a9839e22871457ad
Frank Wu <fwu2012@gmail.com>
2013-02-12 18:48:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LostFound::Application.config.secret_token = '53b779eb6a955873c2e30c339e6b744c91d892eafb164d40b316c65c4cf0263e694250f57b56343e4cd0f79f47a109e8f862c408321c628cc29b9ff6b6229a99'
